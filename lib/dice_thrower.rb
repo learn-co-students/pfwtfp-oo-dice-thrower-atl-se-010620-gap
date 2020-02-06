d58e4ad6fe59715259fc5ccb652f7a47b38eb3a8
Alvin Coulson <aaacoulson@gmail.com>
2020-02-05 20:49:14 -0500

    Done.

--- lib/dice_thrower.rb
+++ lib/dice_thrower.rb
@@ -1,1 +1,18 @@
 # Your code here
+class DiceRoller
+    
+    def initialize(max_pips=6, die_count=2)
+        @max_pips = max_pips
+        @die_count = die_count
+        @dice_set = generate_set
+    end
+
+    def generate_set
+        Array.new(@die_count){ Die.new(@max_pips) }
+    end
+    
+    def dice
+        @dice_set
+    end
+
+end
